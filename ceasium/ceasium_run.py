36f69baabd0edf9f7632707be6ce0a9d66755f71
Evaldas Zmitra <evaldaszmit@gmail.com>
2024-02-11 18:38:47 +0200

    Fix run

--- ceasium/ceasium_run.py
+++ ceasium/ceasium_run.py
@@ -13,8 +13,6 @@ build_folder_name = "build"
 def run(args):
     build_path = os.path.join(args.path, build_folder_name)
     build_config = read_config(args.path)
-    o_files = build_o_files(args.path, build_config, "src")
-    # build_gcc(build_path, o_files, build_config)
-    # exe_path = os.path.join(build_path, build_config["name"])
-    # print_blue("Running...")
-    # run_command(exe_path)
+    exe_path = os.path.join(build_path, build_config["name"])
+    print_blue("Running...")
+    run_command(exe_path)
